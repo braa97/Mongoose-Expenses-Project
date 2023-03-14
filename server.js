e613cd966c82a825e4dcbf4b8a3439063a5acfeb
Braa Knaane <braa97@windowslive.com>
2023-03-14 02:51:28 +0200

    project

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const api = require('./server/routes/api')
 
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/expensesProject", {
+mongoose.connect("mongodb://localhost:27017/expensesProject", {
   useNewUrlParser: true,
 }).catch((err)=> console.log(err))
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -28,22 +28,33 @@ router.post('/expenses', function(req, res) {
     let newExpense = new Expense({ item, amount, group, date })
     newExpense.save().then(() => {
         console.log(`you spent ${amount} on ${item}`);
-        res.status(202).send("Added")
+        res.status(200).send("Added")
     })
 })
 
-router.put('/expenses', function(req, res) {
-    let expense = req.body
-    let item = expense.item;
-    let amount = expense.amount
-    let group = expense.group
-    let date = expense.date ? expense.date : moment().format('YYYY-MM-DD')
+router.put("/update", function (req, res) {
+    let group1 = req.body.group1;
+    let group2 = req.body.group2;
+  
+    Expense.findOneAndUpdate({ group: group1 }, { $set: { group: group2 } })
+      .then((expense) => {
+        res.status(200).send(`Expense ${expense.item} was update from group ${group1} to group ${expense.group}.`);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  });
 
-    let newExpense = new Expense({ item, amount, group, date })
-    newExpense.save().then(() => {
-        console.log(`you spent ${amount} on ${item}`);
-        res.status(202).send("Added")
-    })
-})
+  router.get("/expenses/:group", function (req, res) {
+    let reqGroup = req.params.group
+  
+    Expense.find({group: reqGroup})
+      .then((expenses) => {
+        res.send(expenses)
+      })
+      .catch((err) => {
+        res.status(404).send(err);
+      });
+  });
 
 module.exports = router
